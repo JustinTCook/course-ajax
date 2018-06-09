aa99a1dd7191946ad32fb28c7c20da4d409baa23
Justin Cook <jtcook19@gmail.com>
2018-06-09 13:24:08 -0700

    JQuery solution

--- lesson-2-async-w-jQuery/app.js
+++ lesson-2-async-w-jQuery/app.js
@@ -10,5 +10,50 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        $.ajax({
+            url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+            headers: {
+                Authorization: 'Client-ID d52c4db4f8fc8d2374eefa0105ae87ff3d6dbff26cf4c59d3309e1b9749e4938'
+            }
+        }).done(addImage);
+
+        $.ajax({
+            url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=8976effe535b440e9f1e38486ba3fa29`,
+        }).done(addArticles);
     });
+
+    function addImage(images) {
+        let htmlContent = '';
+
+        if (images && images.results && images.results[0]) {
+            const firstImage = images.results[0];
+            htmlContent = `<figure>
+                    <img src="${firstImage.urls.small}" alt="${searchedForText}">
+                    <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+                </figure>`;
+        } else {
+            htmlContent = '<div class="error-no-image">No images available</div>';
+        }
+
+        responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    }
+
+    function addArticles(articles) {
+        let htmlContent = '';
+
+        if (articles.response && articles.response.docs && articles.response.docs.length > 1) {
+            htmlContent = '<ul>' + articles.response.docs.map(article =>
+                `<li class="article">
+                  <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                  <p>${article.snippet}</p>
+                <li>`)
+                .join('') + '</ul>';
+        } else {
+            htmlContent = '<div class="error-no-articles">No articles available</div>';
+        }
+
+        responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+    }
+
 })();
